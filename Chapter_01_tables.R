be5eb74902f6f1e007c1b7e9cba6ee33f5247085
Funiokova Tatana <fun01@vsb.cz>
2022-01-07 13:01:39 +0100

    tables in Chapter 1

---
+++ Chapter_01_tables.R
@@ -0,0 +1,38 @@
+######################################################
+# Example 1.3                               ##########
+# Employees of a Small Business Company ##############
+EX_DATA_DF <- readRDS("data/EX_DATA_DF.rds")
+# frequency distribution for Degree
+table(EX_DATA_DF$Degree)
+# percent
+prop.table(table(EX_DATA_DF$Degree))*100
+# cumulative frequency
+cumsum(table(EX_DATA_DF$Degree))
+# cumulative percent
+cumsum(prop.table(table(EX_DATA_DF$Degree)))*100
+# contingency table with row and column totals
+addmargins(table(EX_DATA_DF$Gender,EX_DATA_DF$Degree))
+
+# using Tidverse Packages
+library(tidyverse)
+EX_DATA_DF %>% select(Degree) %>%
+    table()
+  
+EX_DATA_DF %>% select(Degree) %>%
+  table() %>%
+  prop.table()  %>% 
+  "*"(100)
+
+EX_DATA_DF %>% select(Degree) %>%
+  table() %>%
+  cumsum()
+
+EX_DATA_DF %>% select(Degree) %>%
+  table() %>%
+  prop.table() %>% 
+  cumsum() %>% 
+  "*"(100)
+ 
+EX_DATA_DF %>% select(Gender,Degree) %>%
+  table() %>%
+  addmargins()
