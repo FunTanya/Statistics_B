d61985c733867a1df791eb9ab705ae43350e2d03
Funiokova Tatana <fun01@vsb.cz>
2022-01-07 16:37:15 +0100

    tables more tidyverse

--- Chapter_01_tables.R
+++ Chapter_01_tables.R
@@ -3,13 +3,15 @@
 # Employees of a Small Business Company ##############
 EX_DATA_DF <- readRDS("data/EX_DATA_DF.rds")
 # frequency distribution for Degree
-table(EX_DATA_DF$Degree)
+(fi = table(EX_DATA_DF$Degree))
 # percent
-prop.table(table(EX_DATA_DF$Degree))*100
+(percent = prop.table(table(EX_DATA_DF$Degree))*100)
 # cumulative frequency
-cumsum(table(EX_DATA_DF$Degree))
+(cum.fi = cumsum(table(EX_DATA_DF$Degree)))
 # cumulative percent
-cumsum(prop.table(table(EX_DATA_DF$Degree)))*100
+(cum.percent = cumsum(prop.table(table(EX_DATA_DF$Degree)))*100)
+# all together
+cbind(fi,percent,cum.fi,cum.percent)
 # contingency table with row and column totals
 addmargins(table(EX_DATA_DF$Gender,EX_DATA_DF$Degree))
 
@@ -32,7 +34,24 @@ EX_DATA_DF %>% select(Degree) %>%
   prop.table() %>% 
   cumsum() %>% 
   "*"(100)
- 
+
+# all together
+EX_DATA_DF %>%
+  group_by(Degree) %>% 
+  summarize(fi=n()) %>% 
+  mutate(percent = round(prop.table(fi)*100,2))%>% 
+  mutate(cum.fi = cumsum(fi)) %>% 
+  mutate(cum.percent = cumsum(percent))
+
+# contingency table with row and column totals
 EX_DATA_DF %>% select(Gender,Degree) %>%
   table() %>%
   addmargins()
+
+library(janitor)
+EX_DATA_DF %>% 
+  group_by(Gender,Degree) %>% 
+  summarise(fij=n(), .groups = 'drop') %>% 
+  spread(Degree,fij) %>% 
+  adorn_totals(name = 'col.sum') %>% 
+  adorn_totals(name = 'row.sum', where = 'col')
